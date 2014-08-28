f5e81db010efa39844903c24571ed3abb7027502
Mirco Kraenz <prosingularity@yahoo.de>
2014-08-28 20:09:10 +0200

    Implemented Graham Scan

---
+++ src/comp_convex_hull/ConvexHull.py
@@ -0,0 +1,78 @@
+'''
+Created on 28.08.2014
+
+@author: proSingularity
+'''
+
+from copy import copy as copy
+from cmath import phase as phase
+
+class ConvexHull(object):
+    '''
+    Operates on complex numbers as if they where 2 dim real-valued vectors, i.e. points in the plane.
+    '''
+
+    def __init__(self, points):
+        '''
+        Constructor
+        '''
+        self.__points = copy(points)
+        
+    def get_points(self):
+        return self.__points
+    
+    def set_points(self, points):
+        self.__points = copy(points)
+        
+    def find_min_y_index(self, points):
+        y_list = [c.imag for c in points]
+        return y_list.index(min(y_list))
+    
+    def normalize(self, origin, points):
+        ''' sets @param origin as new coordinate origin and translates the other points accordingly '''
+        return [c - origin for c in points]
+    
+    def sort_by_phase(self, points):
+        ''' @return: points ordered by their angle w.r.t. to the origin. '''
+        return sorted(points, key=phase)
+    
+    def sort_by_real(self, points):
+        return sorted(points, key=self.__get_real )
+    
+    def ccw(self, a, b, c): 
+        return (b.real-a.real) * (c.imag - a.imag) - (b.imag - a.imag) * (c.real - a.real)
+        
+    def is_ccw(self, a, b, c):
+        return (True if self.ccw(a, b, c) >= 0 else False )
+    
+    def __get_real(self, complex):
+        return complex.real
+    
+    def graham_scan(self):
+        ''' 
+        Perform the Graham scan Algorithm with the point of set, that is given do this ConvexHull instance.
+        @return: A list containing the points on the convex hull.
+        '''
+        points = self.sort_by_real(self.get_points())
+        n = len(points)
+        
+        min_index = self.find_min_y_index(points)
+        origin = points[min_index]
+        points[0], points[min_index] = points[min_index], points[0]
+        
+        points = self.normalize(origin, points)
+        points[1:] = self.sort_by_phase(points[1:])
+        res = points[:] #copies points
+        M = 2
+        
+        for i in range(3,n):
+            while not self.is_ccw(res[M-1],res[M], points[i]):
+                del res[M]
+                M -= 1
+                
+            M += 1
+        return self.normalize(-origin, res)
+            
+                
+        
+        
---
+++ tests/comp_convex_hull/ConvexHullTests.py
@@ -0,0 +1,126 @@
+'''
+Created on 25.08.2014
+
+@author: proSingularity
+'''
+import unittest
+from copy import copy
+from comp_convex_hull.ConvexHull import ConvexHull as ConvexHull
+from random import uniform
+
+
+class ConvexHullTests(unittest.TestCase):
+
+    def get_list_one_point(self):
+        return [1 + 2j]
+
+    def get_list_five_points(self):
+        return [10 + 5j, 0j, -199 + 32j, -10 + 0j, 123 + 1j]
+
+    def get_list_five_points_ordered(self):
+        list_ordered = [0j, -10 + 0j, 123 + 1j, 10 + 5j, -199 + 32j]
+        return list_ordered
+
+    def test___init__(self):
+        points = self.get_list_one_point()
+        hull = ConvexHull(points)
+        self.assertEqual(hull.get_points(), self.get_list_one_point())
+            
+    def point_generator(self, number, radius):
+        ''' generates a list of number many randomized points in the plane with norm <= radius '''
+        return [complex(uniform(-radius, radius), uniform(-radius, radius)) for i in range(number)]
+
+    def test_copy(self):
+        points = self.get_list_one_point()
+        self.assertIsNot(points, copy(points))
+        
+    def test_find_min_y_index_ordered(self):
+        points = self.get_list_five_points_ordered()
+        hull = ConvexHull(points)
+        self.assertEqual(hull.find_min_y_index(points), 0)
+        
+    def test_sort_by_phase_ordered(self):
+        points = [1 + 0j, 1 + 1j, 1 + 2j, 0 + 1j, -1 + 1j, -1 + 0j]
+        hull = ConvexHull(points)
+        self.assertEqual(hull.sort_by_phase(points), points)
+        
+    def test_sort_by_phase_disordered(self):
+        disordered_points = [-1 + 1j, -1 + 0j, 1 + 1j, 1 + 0j, 1 + 2j, 0 + 1j]
+        hull = ConvexHull(disordered_points)
+        solution = [1 + 0j, 1 + 1j, 1 + 2j, 0 + 1j, -1 + 1j, -1 + 0j]
+        self.assertEqual(hull.sort_by_phase(disordered_points), solution)
+        
+    def test_normalize(self):
+        hull = ConvexHull([2 + 2j, 1 + 1j])
+        normalized_points = hull.normalize(1 + 1j, hull.get_points())
+        self.assertEqual(normalized_points, [1 + 1j, 0j])
+        normalized_points = hull.normalize(0j, hull.get_points())
+        self.assertEqual(normalized_points, [2 + 2j, 1 + 1j])
+        
+    def test_sort_by_real(self):
+        points = [1 + 0j, 0j]
+        hull = ConvexHull(points)
+        self.assertEqual(hull.sort_by_real(points), [0j, 1 + 0j])
+        
+    def test_graham_scan_nothing_to_do(self):
+        points = [0j, 2 + 0j, 2 + 2j, 2j]
+        hull = ConvexHull(points)
+        self.assertEqual(hull.graham_scan(), points)
+    
+    def test_graham_scan_easy_sorted_normed(self):
+        points = [0j, 2 + 0j, 1 + 0.5j, 2 + 2j, 2j]
+        hull = ConvexHull(points)
+        self.assertEqual(hull.graham_scan(), [0j, 2 + 0j, 2 + 2j, 2j])
+        
+    def test_graham_scan_easy_sorted_normed2(self):
+        points = [0j, 2 + 0j, 1 + 0.5j, 1 + 0.8j, 2 + 2j, 2j]
+        hull = ConvexHull(points)
+        self.assertEqual(hull.graham_scan(), [0j, 2 + 0j, 2 + 2j, 2j])
+        
+    def test_graham_scan_easy_normed(self):
+        points = [0j, 2 + 2j, 2 + 0j, 1 + 0.5j, 1 + 0.8j, 2j ]
+        hull = ConvexHull(points)
+        self.assertEqual(hull.graham_scan(), [0j, 2 + 0j, 2 + 2j, 2j])
+        
+    def test_graham_scan_easy_normed2(self):
+        points = [ 2 + 2j, 2 + 0j, 1 + 0.5j, 1 + 0.8j, 2j, 0j ]
+        hull = ConvexHull(points)
+        self.assertEqual(hull.graham_scan(), [0j, 2 + 0j, 2 + 2j, 2j])
+        
+    def test_graham_scan_easy_normed3(self):
+        points = [ 2 + 2j, -2 + 0j, 1 + 0.5j, 1 + 0.8j, 2j, 0j ]
+        hull = ConvexHull(points)
+        self.assertEqual(hull.graham_scan(), [-2 + 0j, 0j, 1 + 0.5j, 2 + 2j, 2j])
+        
+    def test_graham_scan_easy(self):
+        points = [ 2 + 2j, -2 + 0j, 1 + 0.5j, 1 + 0.8j, 2j]
+        hull = ConvexHull(points)
+        self.assertEqual(hull.graham_scan(), [-2 + 0j, 1 + 0.5j, 2 + 2j, 2j])
+        
+    def test_graham_scan_one_point(self):
+        hull = ConvexHull([1 + 1j])
+        self.assertEqual(hull.graham_scan(), [1 + 1j])
+           
+    def test_graham_scan_two_points(self):
+        hull = ConvexHull([1 + 1j, 2 + 0.5j])
+        self.assertEqual(hull.graham_scan(), [2 + 0.5j, 1 + 1j])
+    
+    def test_graham_scan_two_lin_dependant_points_sorted(self):
+        points = [0j, 1 + 1j, 2 + 2j, 2j]
+        hull = ConvexHull(points)
+        self.assertEqual(hull.graham_scan(), points)
+        
+    def test_graham_scan_two_lin_dependant_points(self):
+        hull = ConvexHull([0j, 2 + 2j, 1 + 1j, 2j])
+        self.assertEqual(hull.graham_scan(), [0j, 1 + 1j, 2 + 2j, 2j])
+
+    def test_graham_scan_many_uninteresting_points(self):
+        solution = [0+-2j, 2 + 0j, 2j, -2 + 0j]
+        l = self.point_generator(10000, 1) + solution
+        hull = ConvexHull(l)
+        self.assertEqual(hull.graham_scan(), solution)
+        
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
